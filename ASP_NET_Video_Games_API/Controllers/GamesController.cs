3c5fe17a23b69f6a307f565931a0e41eae86fb0e
Amanda <aclaas522@gmail.com>
2022-04-26 13:45:01 -0500

    Created GetbyId Endpoint and GetGames Endpoint

--- ASP_NET_Video_Games_API/Controllers/GamesController.cs
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -15,32 +15,39 @@ namespace ASP_NET_Video_Games_API.Controllers
             _context = context;
         }
 
-        [HttpGet]
-        public IActionResult GetPublishers()
-        {
-            var videoGamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
-            return Ok(videoGamePublishers);
-        }
+        //[HttpGet]
+        //public IActionResult GetPublishers()
+        //{
+        //    var videoGamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
+        //    return Ok(videoGamePublishers);
+        //}
 
-        [HttpGet("{pubName}")]
-        public IActionResult GetGamesByPublisher(string pubName)
-        {
-            //int? maxYear = _context.VideoGames.Select(vg => vg.Year).Max();
-            //int? minYear = _context.VideoGames.Select(vg => vg.Year).Min();
-            var videoGames = _context.VideoGames.Where(vg => vg.Publisher == pubName);
-            return Ok(videoGames);
-        }
+        //[HttpGet("{pubName}")]
+        //public IActionResult GetGamesByPublisher(string pubName)
+        //{
+        //    //int? maxYear = _context.VideoGames.Select(vg => vg.Year).Max();
+        //    //int? minYear = _context.VideoGames.Select(vg => vg.Year).Min();
+        //    var videoGames = _context.VideoGames.Where(vg => vg.Publisher == pubName);
+        //    return Ok(videoGames);
+        //}
 
         [HttpGet("{id}")]
 
-        public IActionResult GetGames(int id)
+        public IActionResult GetGamesById(int id)
         {
 
-            var videoGamesById = _context.VideoGames.Select(vgi => vgi.Id == id);
-            return Ok(videoGamesById);
+            var videoGames = _context.VideoGames.Where(vg => vg.Id == id);
+            return Ok(videoGames);
 
         }
 
+        //[HttpGet]
+
+        //public IActionResult GetGames()
+        //{
+        //    var videoGames = _context.VideoGames;
+        //    return Ok(videoGames);
+        //}
 
     }
 }
